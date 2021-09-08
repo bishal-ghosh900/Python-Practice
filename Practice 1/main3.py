a4ec7dad5cc5203441a330d140b9609c82103413
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:08:11 +0530

    String

---
+++ Practice 1/main3.py
@@ -0,0 +1,12 @@
+#String
+
+name = "My name is Bishal Ghosh"
+
+print(name[0])  # M
+print(name[-2]) # h
+print(name[0:5])# My na
+print(name[1:]) #  y name is Bishal Ghosh
+print(name[:len(name) - 1]) # My name is Bishal Ghos
+print(name[:])  #  My name is Bishal Ghosh
+print(name[1:-1])  #  y name is Bishal Ghos
+

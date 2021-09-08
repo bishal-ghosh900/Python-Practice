0abf070695a19ba020ecd7a4449bfea59621ad8d
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:18:24 +0530

    list methods

---
+++ Practice 1/main20.py
@@ -0,0 +1,41 @@
+# list methods
+numbers = [5, 2, 1, 7, 4]
+
+numbers.append(10) # [5, 2, 1, 7, 4, 10]
+print(numbers)
+
+numbers.insert(2, 6) # [5, 2, 6, 1, 7, 4, 10]
+print(numbers)
+
+numbers.remove(10) # [5, 2, 6, 1, 7, 4]
+print(numbers)
+
+numbers.pop() # [5, 2, 6, 1, 7]
+print(numbers)
+
+
+print(numbers.index(6)) # 2 --> first occurence of 6 is in index 2
+
+print(6 in numbers) # True
+print(10 in numbers) # False
+
+numbers.append(6) # [5, 2, 6, 1, 7, 6]
+print(numbers)
+
+print(numbers.count(6)) # 2 --> all occurence of 6 in the list
+
+numbers.sort() # [1, 2, 5, 6, 6, 7]
+print(numbers)
+
+numbers.reverse() # [7, 6, 6, 5, 2, 1]
+print(numbers)
+
+temp_numbers = numbers.copy();
+print(temp_numbers)
+
+temp_numbers.append(33)
+print(temp_numbers) # [7, 6, 6, 5, 2, 1, 33]
+print(numbers) # [7, 6, 6, 5, 2, 1]
+
+numbers.clear() # []
+print(numbers)

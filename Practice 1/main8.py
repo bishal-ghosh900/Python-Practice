55fa405affe386fc883220dcfdc44bf52553fa0c
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:11:11 +0530

    If else Statement

---
+++ Practice 1/main8.py
@@ -0,0 +1,11 @@
+# If else Statement
+
+num1 = input("Give me number1 value: ")
+num2 = input("Give me number2 value: ")
+
+if num1 > num2 : 
+    print(f"{num1} is greater than {num2}")
+elif (num1 < num2):
+    print(f"{num2} is greate than {num1}")
+else:
+    print("Both are equal")

c7f05350ad912e21bc4ae2af9f3d2aae951b5782
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:10:30 +0530

    Arithmatic Operator

---
+++ Practice 1/main6.py
@@ -0,0 +1,22 @@
+# Arithmatic Operator 
+
+n1 = 10
+n2 = 3
+
+print(n1 / n2)  # 3.3333333333333335 --> It is the float division
+print(n1 // n2) # 3 --> It is the integer division
+print(n1 ** n2) # 1000 --> It the to the power sign
+
+print("*" * 5) # *****
+print("*" * 4) # ****
+print("*" * 3) # ***
+print("*" * 2) # **
+print("*" * 1) # *
+
+
+# Arithmatic order of precendence -->
+# exponentiation (Ex: 2 ** 3) >
+# multiplication or division >
+# addition or subtraction
+# Example --> 10 + 3 * 2 ** 2 = 22
+

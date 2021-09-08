3c567c67bf94a63bab28e2f189d06644c8421fd3
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:10:52 +0530

    Math functions

---
+++ Practice 1/main7.py
@@ -0,0 +1,11 @@
+# Math functions
+import math
+
+
+x1 = -4.6
+x2 = 4.6
+print(round(x2)) # 5
+print(abs(x1)) # 4.5
+print(math.ceil(x2)) # 5
+print(math.floor(x2)) # 4
+print(math.factorial(math.floor(x2))) # 24

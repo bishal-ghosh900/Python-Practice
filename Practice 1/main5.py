7f6b6a6f57f810a885a8375414ce5b65813f0c08
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:10:03 +0530

    String methods

---
+++ Practice 1/main5.py
@@ -0,0 +1,15 @@
+# String methods
+
+name = "I am Bishal Ghosh"
+
+name_length = len(name)
+
+print(name.upper()) # I AM BISHAL GHOSH
+print(name.lower()) # i am bishal ghosh
+print(name.capitalize()) # I am bishal ghosh
+print(name.find("Ghosh")) # 12
+print(name.replace("s", "m")) # I am Bimhal Ghomh
+print(name.title()) # I Am Bishal Ghosh
+print(name.split(' ')) # ['I', 'am', 'Bishal', 'Ghosh']
+print("Bishal" in  name) # True
+

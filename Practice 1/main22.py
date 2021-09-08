4a39422d17d76a56846ae297a9dacaa586cb708a
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:19:02 +0530

    Tuples

---
+++ Practice 1/main22.py
@@ -0,0 +1,8 @@
+# Tuples
+
+# Tuples are immutable list, so we can't modify it after creating it
+
+tuple = (1, 2, 3, 4, 5, 6, 3, 4, 1)
+
+print(tuple.count(4)) # 2 -> 2 occurence of 4
+print(tuple) # (1, 2, 3, 4, 5, 6, 3, 4, 1)

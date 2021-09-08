f4da2cf8a7217389dd00bb1127779a4293faecad
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-09 00:13:32 +0530

    For loop

---
+++ Practice 1/main14.py
@@ -0,0 +1,14 @@
+# For loop
+
+for i in "Bishal":
+    print(i, end = " ")
+print("\n###########################")
+for i in [1, 2, 3, "Bishal", "Ghosh"]:
+    print(i)
+print("###########################")
+for i in range(10):
+    print(i, end=" ") # 0 to till 9 incrementing by 1 in each iteration 
+print("\n###########################")
+for i in range(1, 10, 2):
+    print(i, end=" ") # 1 to till 9 incrementing by 2 in each iteration 
+    

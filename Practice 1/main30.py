817449ef838475d6cb5861f20f63f08852a39a5f
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-14 19:40:42 +0530

    Sieve of Eratosthenes

--- Practice 1/main30.py
+++ Practice 1/main30.py
@@ -12,7 +12,11 @@ def findPrimes(n):
             while j * i <= n:
                 arr[j*i] = 0
                 j += 1
-    for index, value in enumerate(arr):
+    for index, value in enumerate(arr): # enumerate will give tuples for every iteration which will contain index and value of that particular iteration coming from the list . And in this particular list the tuple is being unpacked in the index and the value variable
+    
+    # Its something like --> index, value = ("0", 1)
+    #                        index, value = ("1", 2) and so on
+    # "0", "1", "2" etc are the indexs of the list, and the next argument is the value of that particular index from the list in which we are performing enumerate() operation.
         if value == 1:
             print(index, sep=" ")
         else:

41d6d84698c66e11365515c8580ea878f35c40b9
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-14 23:11:20 +0530

    Dictionary

--- Practice 1/main24.py
+++ Practice 1/main24.py
@@ -6,6 +6,7 @@ emp = {
     "last_name": "Ghosh",
 }
 
+
 # key should be unique
 
 print(emp) # {'id': 101, 'first_name': 'Bishal', 'last_name': 'Ghosh'}
@@ -23,3 +24,22 @@ print(emp.get("address")) # None --> None is an object which represent abscence 
 
 print(emp.get("address", "Kolkata")) # Kolkata
 print(emp) # {'id': 102, 'first_name': 'Bishal', 'last_name': 'Ghosh'} --> there is not any "address key"
+
+
+Points = dict(x = 1, y = 2, z = 3 )
+print(Points) # {'x': 1, 'y': 2, 'z': 3}
+
+# looping through dictionary 
+for i in Points:
+    print(i,Points[i])
+# output ->
+# x 1
+# y 2
+# z 3
+
+for key , value in Points.items():
+    print(key, value)
+#output ->
+# x 1
+# y 2
+# z 3

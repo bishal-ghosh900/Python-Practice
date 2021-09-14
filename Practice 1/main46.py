02344580c4112a1e8862c7a60830033b27578c5f
bishal-ghosh900 <ghoshghoshbishal@gmail.com>
2021-09-15 00:48:00 +0530

    find the most repeated character in the sentence

--- Practice 1/main46.py
+++ Practice 1/main46.py
@@ -1,7 +1,9 @@
 # find the most repeated character in the sentence
+from pprint import pprint
 
 name = "This is a common interview question"
 
+# method 1 ->
 
 max = -1
 j = -1
@@ -12,3 +14,20 @@ for i in name:
         max = count
         j = name.index(i)
 print(f"max character is {name[j]} counting is {max}")
+
+
+# method 2 ->
+
+nameDt = {}
+
+for char in name:
+    nameDt[char] = (nameDt[char] + 1) if (char in nameDt) else 1
+
+pprint(nameDt, width = 1)
+
+nameDt_real = sorted(nameDt.items(), key= lambda key:key[1], reverse=True)
+
+print(nameDt_real)
+
+
+print(f"The pair of the max occured character and its occurence is" , nameDt_real[0])
